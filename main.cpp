f103b89ee919b6183cf825f9fd15cd58b030ac3b
Roy Tu <thatsnail@gmail.com>
2012-03-28 18:55:51 -0400

    Optimized; beta template code; screen wrapping

--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
 #include <SDL.h>
 #include <vector>
 
-const int WIDTH = 20;
-const int HEIGHT = 20;
+const int WIDTH = 128;
+const int HEIGHT = 96;
 
 const int XSCALE = 8;
 const int YSCALE = 8;
 
-const int FREQ_SPAWN = 8; //frequency = 1 per FREQ_SPAWN squares
+const int FREQ_SPAWN = 4; //frequency = 1 per FREQ_SPAWN squares
 
-int getNeighbors(std::vector< std::vector<bool> > g, int x, int y);
+const int DIRECTIONS[8][2]={{-1,-1},{0,-1},{1,-1},{-1,0},{1,0},{-1,1},{0,1},{1,1}};
+
+int getNeighbors(int x, int y);
 void updateGrid();
+void updateGridNeighbors();
 void drawScreen();
 void randomizeGrid();
+template<typename T> T getCell(std::vector< std::vector<T> > g, int x, int y);
+template<typename T> void setCell(std::vector< std::vector<T> >* g, int x, int y, T value);
 
 void FillRect(int x, int y, int w, int h, int color); // patrick - http://pbeblog.wordpress.com/2009/06/24/drawing-rectangles-sdl/
 
 SDL_Surface* screen=NULL;
 std::vector< std::vector<bool> > grid (WIDTH, std::vector<bool>(HEIGHT));
 std::vector< std::vector<bool> > gridnew (WIDTH, std::vector<bool>(HEIGHT));
+std::vector< std::vector<int> > gridneighbors (WIDTH, std::vector<int>(HEIGHT, 0));
 Uint32* pixels;
 
 int main( int argc, char* args[])
@@ -59,79 +65,60 @@ int main( int argc, char* args[])
 	return 0;
 }
 
-int getNeighbors(std::vector< std::vector<bool> > g,int x,int y)
-{
-	int neighbors=0;
-	if(x>0 && y>0)
-	{
-		if(g[x-1][y-1])
-		{
-			neighbors++;
-		}
-	}
-	if(y>0)
-	{
-		if(g[x][y-1])
-		{
-			neighbors++;
-		}
-	}
-	if(x<WIDTH-1 && y>0)
-	{
-		if(g[x+1][y-1])
-		{
-			neighbors++;
-		}
-	}
-
-	if(x>0)
-	{
-		if(g[x-1][y])
-		{
-			neighbors++;
-		}
-	}
-	if(x<WIDTH-1)
-	{
-		if(g[x+1][y])
-		{
-			neighbors++;
-		}
-	}
-
-	if(x>0 && y<HEIGHT-1)
-	{
-		if(g[x-1][y+1])
-		{
-			neighbors++;
-		}
-	}
-	if(y<HEIGHT-1)
-	{
-		if(g[x][y+1])
-		{
-			neighbors++;
-		}
-	}
-	if(x<WIDTH-1 && y<HEIGHT-1)
-	{
-		if(g[x+1][y+1])
-		{
-			neighbors++;
-		}
-	}
-
-	return neighbors;
-}
-
-void updateGrid()
+void updateGridNeighbors()
 {
 	for(int x=0;x<WIDTH;x++)
 	{
 		for(int y=0;y<HEIGHT;y++)
 		{
+			gridneighbors[x][y]=0;
+		}
+	}
+	for(int x=0;x<WIDTH;x++)
+	{
+		for(int y=0;y<HEIGHT;y++)
+		{
+			if(grid[x][y])
+			{
+				for(int d=0;d<8;d++)
+				{
+					int dx = DIRECTIONS[d][0];
+					int dy = DIRECTIONS[d][1];
+					int ov = gridneighbors[((x+dx)+WIDTH)%WIDTH][((y+dy)+HEIGHT)%HEIGHT];
+					gridneighbors[((x+dx)+WIDTH)%WIDTH][((y+dy)+HEIGHT)%HEIGHT]=ov+1;
+				}
+			}
+		}
+	}
+}
+
+template<typename T> T inline getCell(std::vector< std::vector<T> > g, int x, int y)
+{
+	return g[(x+WIDTH)%WIDTH][(y+HEIGHT)%HEIGHT];
+}
+
+template<typename T> void inline setCell(std::vector< std::vector<T> >* g, int x, int y, T value)
+{
+	int xx=(x+WIDTH)%WIDTH;
+	int yy=(y+HEIGHT)%HEIGHT;
+	std::vector< std::vector<T> > gp = *g;
+	gp[xx][yy]=value;
+}
+
+int getNeighbors(int x,int y)
+{
+	return gridneighbors[x][y];
+}
+
+void updateGrid()
+{
+	updateGridNeighbors();
+	for(int x=0;x<WIDTH;x++)
+	{
+		for(int y=0;y<HEIGHT;y++)
+		{
 			gridnew[x][y]=grid[x][y];
-			int n = getNeighbors(grid,x,y);
+			int n = getNeighbors(x,y);
 			if(grid[x][y]) //live cell
 			{
 				if(n<2 || n>3)
